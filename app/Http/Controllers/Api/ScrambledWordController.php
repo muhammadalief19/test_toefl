f76a03bade469e96674300983aa3b2cd763b4c53
Reza1290 <reza.muktasib@gmail.com>
2024-05-26 03:58:41 +0700

    add: track scrambled

--- app/Http/Controllers/Api/ScrambledWordController.php
+++ app/Http/Controllers/Api/ScrambledWordController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\ScrambledClaim;
 use App\Models\Word;
 use Exception;
 use Illuminate\Http\Request;
@@ -26,5 +27,34 @@ class ScrambledWordController extends Controller
         }
         
         
+    }
+
+    public function store(Request $request){
+        try{
+            $request->validate([
+                'word_id' => 'required',
+                'is_true' => 'required',
+            ]);
+
+            $user = auth()->user();
+            $scrambledClaim = new ScrambledClaim();
+
+
+            $scrambledClaim->word_id = $request->word_id;
+            $scrambledClaim->is_true = $request->is_true;
+            $scrambledClaim->user_id = $user->_id;
+
+            $scrambledClaim->save();
+
+            return response()->json([
+                'success'=> true,
+                'data' => true
+            ]);
+        }catch(Exception $e){
+            return response()->json([
+                'success' => false,
+                'data'=> $e->getMessage()
+            ]);
+        }
     }
 }
