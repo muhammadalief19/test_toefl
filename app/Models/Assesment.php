1635b98284a8e71742fdb3cc1f6493df3d7b920e
muhammadalief19 <aliefmuhammad1908@gmail.com>
2024-09-02 15:27:20 +0700

    fix: fix classname

---
+++ app/Models/Assesment.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Assesment extends Model
+{
+    use HasFactory;
+
+    protected $connection = 'mongodb';
+    protected $collection = 'assessments';
+
+    protected $fillable = [
+        'user_id',
+        'assessment_type',
+        'score',
+        'result',
+        'assessment_date'
+    ];
+}
