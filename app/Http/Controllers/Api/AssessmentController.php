3858d204765390654f6db71d1f323a1873a3de94
muhammadalief19 <aliefmuhammad1908@gmail.com>
2024-09-20 15:06:33 +0700

    feat: membuat AssessmentController

---
+++ app/Http/Controllers/Api/AssessmentController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Assesment;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class AssessmentController extends Controller
+{
+    public function store(Request $request)
+    {
+        date_default_timezone_set('Asia/Jakarta');
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'nullable',
+            'assessment_type' => 'nullable',
+            'score' => 'nullable',
+            'result' => 'nullable|json',
+            'assesment_date' => 'nullable',
+        ]);
+
+        $date = new Carbon();
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $createData = Assesment::create([
+            'user_id' => $request->user_id,
+            'assessment_type' => $request->assessment_type,
+            'score' => $request->score,
+            'result' => $request->score,
+            'assesment_date' => $date->now()->isoFormat('Y-M-D H:mm:ss'),
+        ]);
+
+        if ($createData) {
+            return response()->json(
+                [
+                    'success' => true,
+                    'message' => 'Assessment Berhasil Ditambahkan',
+                ],
+                201,
+            );
+        } else {
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'Assessment Gagal Ditambahkan',
+                ],
+                500,
+            );
+        }
+    }
+}
